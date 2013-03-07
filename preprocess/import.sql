bdfeb625d05de48137859e9675902567fddba41e
Justin Huang <jstn@cs.washington.edu>
2013-03-06 20:22:19 -0800

    Derby import script creates table and index in addition to importing.

--- preprocess/import.sql
+++ preprocess/import.sql
@@ -1,1 +1,18 @@
-CALL SYSCS_UTIL.SYSCS_IMPORT_TABLE(null, 'CROSSWIKIS', 'crosswikis.tsv', '	', null, null, 0)
+CONNECT 'jdbc:derby:crosswikis;create=true';
+CREATE TABLE crosswikis (
+  anchor VARCHAR(32000), 
+  cprob DOUBLE,
+  entity VARCHAR(32000),
+  info VARCHAR(32000),
+  count INTEGER
+);
+CALL SYSCS_UTIL.SYSCS_IMPORT_TABLE(
+  null,
+  'CROSSWIKIS',
+  'crosswikis.tsv',
+  '	',
+  null,
+  null,
+  0
+);
+CREATE INDEX lnrmanchors ON crosswikis (anchor);
