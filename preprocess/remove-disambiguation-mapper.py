b982996fbea05213cbdddec7fdc27561e0e15b48
Justin Huang <jstn@cs.washington.edu>
2013-03-06 18:15:37 -0800

    remove-disambiguation-mapper also removes rows with 0 counts.

--- preprocess/remove-disambiguation-mapper.py
+++ preprocess/remove-disambiguation-mapper.py
@@ -5,6 +5,7 @@ is probably the best we can do without going to the Wikipedia API.
 import sys
 
 ENTITY_COL = 2
+COUNT_COL = 4
 
 def main():
   """Assumes the line is tab-separated, with columns: anchor, cprob, entity,
@@ -12,7 +13,8 @@ def main():
   for line in sys.stdin:
     columns = line.split('\t')
     entity = columns[ENTITY_COL]
-    if not entity.endswith('_(disambiguation)'):
+    count = int(columns[COUNT_COL])
+    if (not entity.endswith('_(disambiguation)')) and (not count == 0):
       print line,
 
 if __name__ == '__main__':
